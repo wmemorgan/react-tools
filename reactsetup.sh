9b13073cf910c865e47b24f41a3e4399f6667f59
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-07 23:59:12 +0800

    Refactor base project setup
    Add project directory variable

--- reactsetup.sh
+++ reactsetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Define react-tools director
 REACT_TOOLS_DIR="$HOME/devtools/react-tools"
+echo "React Tools directory:"
+echo $REACT_TOOLS_DIR
 
 # Determine project settings and features
 echo "Project name (no spaces):"
@@ -16,15 +18,22 @@ read styled
 
 # Setup project directory and base React boilerplate files
 echo "Setup project files"
-create-react-app $projectname && cd $projectname && mkdir src/components &&
+create-react-app $projectname &&
+cd $projectname
+mkdir src/components
+PROJECT_DIR=$(pwd)
+echo "Project directory:"
+echo $PROJECT_DIR
 echo "Copying custom favicon"
-cp $REACT_TOOLS_DIR/templates/public/favicon.ico $projectname/public/favicon.ico
+cp $REACT_TOOLS_DIR/templates/public/favicon.ico $PROJECT_DIR/public/
 
 if [ $spa = 'y' ]; then
   echo "Installing react-router-dom"
-  yarn add react-router-dom &&
-  mkdir src/routes && touch src/routes/index.js
-  mkdir src/app && touch src/app/index.js
+  yarn add react-router-dom
+  mkdir $PROJECT_DIR/src/routes
+  touch $PROJECT_DIR/src/routes/index.js
+  mkdir $PROJECT_DIR/src/app
+  touch $PROJECT_DIR/src/app/index.js
   echo "Created routes and app directories"
 fi 
 
@@ -36,8 +45,8 @@ fi
 if [ $redux = 'y' ]; then
   echo "Installing Redux components"
   yarn add redux redux-thunk react-redux redux-logger &&
-  mkdir src/config && touch src/config/store.js &&
-  mkdir src/actions src/reducers && touch src/reducers/index.js
+  mkdir $PROJECT_DIR/src/config && touch $PROJECT_DIR/src/config/store.js &&
+  mkdir $PROJECT_DIR/src/actions $PROJECT_DIR/src/reducers && touch $PROJECT_DIR/src/reducers/index.js
   echo "Created store, actions, reducers directories"
 fi
 
@@ -45,7 +54,11 @@ if [ $styled = 'y' ]; then
   echo "Installing styled-components"
   yarn add styled-components styled-reset &&
   echo "Copying custom design components"
-  cp $REACT_TOOLS_DIR/templates/components/DesignComponents $projectname/src/components/
+  cp $REACT_TOOLS_DIR/templates/components/DesignComponents $PROJECT_DIR/src/components/
   echo "Copying custom shared components"
-  cp $REACT_TOOLS_DIR/templates/components/SharedComponents $projectname/src/components/
+  cp $REACT_TOOLS_DIR/templates/components/SharedComponents $PROJECT_DIR/src/components/
 fi
+
+echo "List project directory contents"
+ls $PROJECT_DIR
+echo "Setup complete"
